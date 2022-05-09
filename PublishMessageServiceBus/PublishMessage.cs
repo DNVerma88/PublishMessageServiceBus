decad8236a999783c7da4d190db9df32aec47a16
Narayan, Deep <Deep.Narayan@BakerHill.com>
2022-05-09 18:12:04 +0530

    commit changes

--- PublishMessageServiceBus/PublishMessage.cs
+++ PublishMessageServiceBus/PublishMessage.cs
@@ -19,15 +19,21 @@ namespace PublishMessageServiceBus
         public async Task PublishMessageToTopic()
         {
             await using var client = new ServiceBusClient(connectionString);
-            // create the sender
             sender = client.CreateSender(topicName);
 
-            // create a message that we can send. UTF-8 encoding is used when providing a string.
-            ServiceBusMessage message = new ServiceBusMessage("Hello world!");
-            message.PartitionKey = "Testing_Publisher";
+            ServiceBusMessage message = new ServiceBusMessage("Pushing message for consumer 1.");
+            message.PartitionKey = "Subscriber1";
             message.MessageId = Guid.NewGuid().ToString();
-            // send the message
+            message.Subject = "consumer1"; // Added filter through portal on subject column for subscription1
+            // send the message in topic for subscription1 
             await sender.SendMessageAsync(message);
+
+            ServiceBusMessage message2 = new ServiceBusMessage("Pushing message for consumer 2.");
+            message2.PartitionKey = "Subscriber2";
+            message2.MessageId = Guid.NewGuid().ToString();
+            message2.Subject = "consumer2"; // Added filter through portal on subject column for subscription2
+            // send the message in topic for subscription2 
+            await sender.SendMessageAsync(message2);
 
 
 
