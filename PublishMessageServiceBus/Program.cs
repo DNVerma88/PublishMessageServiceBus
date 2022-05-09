fb01d64cc7d3389b4a53b4d88acf54850fec351b
Narayan, Deep <Deep.Narayan@BakerHill.com>
2022-05-09 11:31:10 +0530

    commit changes

--- PublishMessageServiceBus/Program.cs
+++ PublishMessageServiceBus/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PublishMessageServiceBus;
+
+PublishMessage publishMessage = new PublishMessage();
+await publishMessage.PublishMessageToTopic();
---
+++ PublishMessageServiceBus/PublishMessage.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublishMessageServiceBus
+{
+    public class PublishMessage
+    {
+        static string connectionString = "Endpoint=sb://notificationservice0.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=b4ojYUDXIBKvtL6s1IN4JsNHUvG/KGVzIv33ta0nBHg=";
+
+        static string topicName = "notification0";
+
+        static ServiceBusSender sender;
+
+
+        public async Task PublishMessageToTopic()
+        {
+            await using var client = new ServiceBusClient(connectionString);
+            // create the sender
+            sender = client.CreateSender(topicName);
+
+            // create a message that we can send. UTF-8 encoding is used when providing a string.
+            ServiceBusMessage message = new ServiceBusMessage("Hello world!");
+            message.PartitionKey = "Testing_Publisher";
+            message.MessageId = Guid.NewGuid().ToString();
+            // send the message
+            await sender.SendMessageAsync(message);
+
+
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+
+    }
+}
